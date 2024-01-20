997197ea89eea6db8d957f300dc798b94623969e
iwayemi-kehinde <iwayemikehinde1@gmail.com>
2024-01-20 03:10:12 +0100

    first commit

---
+++ Assets/script.js
@@ -0,0 +1,54 @@
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
+const generate = document.getElementById("generate")
+const copy = document.getElementById("copy")
+const loader = document.getElementById("loader")
+const API_URL = "https://api.quotable.io/random"
+async function getQuote(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+  quote.innerHTML = data.content
+  author.innerHTML = data.author
+}
+getQuote(API_URL)
+tweet.onclick = function () {
+  window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "----" + author.innerHTML, "Tweet Window", "width=600", "height=400")
+}
+generate.onclick = function () {
+  getQuote(API_URL)
+}
+
+generate.onclick = () => {
+  copy.innerHTML = "Copy"
+}
+
+
+setTimeout(() => {
+  loader.classList.add("show")
+}, 3000);
+
+
+function copyTextToClipboard() {
+  var textToCopy = `"${quote.textContent}" by ${author.textContent}`;
+  var textarea = document.createElement("textarea");
+  textarea.value = textToCopy;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy")
+  document.body.removeChild(textarea);
+  copy.innerHTML = "Copied!"
+}
+
+
+copy.addEventListener("click", function () {
+  copyTextToClipboard()
+})
+
+if (navigator.isonLine === true) {
+  quote.innerHTML = data.content
+  author.innerHTML = data.author
+} else {
+  quote.innerHTML = "CONNECT YOUR DEVICE TO INTERNET"
+  author.innerHTML = "NOT CONNECTED"
+}
+
